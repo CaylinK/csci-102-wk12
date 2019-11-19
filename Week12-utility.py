d9d12c1d978f3bb0a4fd47a5b0a3e1a31983ef8f
Caylin Kuenn <ekuenn@mines.edu>
2019-11-18 20:47:34 -0700

    Added 2nd function

--- Week12-utility.py
+++ Week12-utility.py
@@ -2,9 +2,30 @@
 # CSCI 102 - Section B
 # Week 12 - Part A
 
+# First commit done here---
+
 # Function # 1
 def PrintOutput(output):
     print("OUTPUT",output)
 
 # Second Commit done here---
 
+# Function #2
+def LoadFile(file):
+    with open(file,'r') as text:
+        string = list(text.read())
+        Total = ""
+        Array = []
+
+        for word in string:  # Place each word in one cell of array
+
+            if word == '\n':
+                word = ""
+                Array.append(Total)
+                Total = ""
+
+            Total += word
+
+        print(Array)
+
+# Commit #3 done here ----
